0b1966fedaec170b3dbb0a43571710c88e925ce9
Caleb Braun <Caleb@Caleb-Brauns-MacBook.local>
2017-04-24 17:48:25 -0500

    worked on line interactions

--- parallel.js
+++ parallel.js
@@ -12,22 +12,22 @@ margin = 0;    // Margin around visualization
 
 var headers =
 [
-  "num_o_ring_distress",
-  "launch_temp",
-  "leak_check_pressure",
-  "tufte_metric"
+  "RH",
+  "wind",
+  "area",
+  "temp"
 ];
 
 var axisTitles = {
-    "num_o_ring_distress": "Number Distressed O-Rings",
-    "launch_temp": "Launch Temperature (F)",
-    "leak_check_pressure": "Leak Check Pressure (psi)",
-    "tufte_metric": "Tufte Damage Metric"
+    "RH": "Humidity",
+    "wind": "Wind Speed (kph)",
+    "area": "Area Burned (ha)",
+    "temp": "Temperature (C)"
 };
 
 yScales = [];
 
-d3.csv("challenger.csv", function(csvData) {
+d3.csv("forestfires.csv", function(csvData) {
   var svg1 = d3.select('#svg1').attr('width', w).attr('height', h);
 
   // Build the axes
@@ -42,29 +42,17 @@ d3.csv("challenger.csv", function(csvData) {
   	  		.ticks(5);
       yAxisG = svg1.append('g')
   	  		.attr('class', 'axis')
-  	  		.attr('transform', 'translate(' + (i / headers.length) * w + ',0)')
+  	  		.attr('transform', 'translate(' + (i / (headers.length-1)) * w + ',0)')
   	  		.call(yAxis);
       yLabel = svg1.append('text')
   	  		.attr('class','label')
   	  		.attr('x', yOffset/2)
   	  		.attr('y', h/2-20)
             .style('text-anchor', 'middle')
-            .attr('transform', 'rotate(-90,' + String(i / headers.length) * w + ',0)')
+            .attr('transform', 'rotate(-90,' + String(i / (headers.length-1)) * w + ',0)')
   	  		.text(axisTitles[headers[i]]);
 
       yScales.push(yScale);
-
-      // svg1.append('text')
-      //     .attr('x', (i / headers.length) * w)
-      //     .attr('y', h)
-      //     .text(axisTitles[headers[i]]);
-      // svg1.append('line')
-      //     .attr('stroke', 'black')
-      //     .attr('stroke-width', 2)
-      //     .attr('x1', (i / headers.length) * w)
-      //     .attr('y1', 0)
-      //     .attr('x2', (i / headers.length) * w)
-      //     .attr('y2', h);
   }
 
   var path = svg1.selectAll('path').data(csvData);
@@ -73,18 +61,26 @@ d3.csv("challenger.csv", function(csvData) {
       .append('svg:path')
       .attr('d', generatePath)
       .attr('fill', 'none')
-      .attr('stroke', 'black');
-
-  console.log(yScales);
-
+      .attr('stroke', 'black')
+      .on('mouseover', function() {
+          d3.select(this).attr({
+              "stroke": "deepskyblue",
+              "stroke-width": 4
+          });
+      })
+      .on('mouseout', function() {
+          d3.select(this).attr({
+              "stroke": "black",
+              "stroke-width": 1
+          });
+      });
 });
 
 function generatePath(d) {
-  //d="M150 0 L75 200 L225 200
   var pathString = "";
   for (var i = 0; i < headers.length; i++) {
     pathString += (i == 0) ? "M" : "L";
-    pathString += (i / headers.length) * w + " " + yScales[i](d[headers[i]]) + " ";
+    pathString += (i / (headers.length-1)) * w + " " + yScales[i](d[headers[i]]) + " ";
   }
   return pathString
 }
